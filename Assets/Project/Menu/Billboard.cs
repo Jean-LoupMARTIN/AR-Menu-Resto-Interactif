88276deceded9dd35db61235f556fd5879e34529
Jean-LoupMARTIN <jean_loup_martin@hotmail.com>
2020-12-12 21:26:28 +0100

    Import

---
+++ Assets/Project/Menu/Billboard.cs
@@ -0,0 +1,14 @@
+﻿
+
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform camTransform;
+
+    Quaternion originalRotation;
+
+    void Start() { originalRotation = transform.rotation; }
+
+    void Update() { transform.rotation = camTransform.rotation * originalRotation; }
+}
---
+++ Assets/Project/Menu/Food.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Food : MonoBehaviour
+{
+    public List<GameObject> foods;
+    public List<string> names;
+    public Text name;
+    private int i = 0;
+    private GameObject food;
+
+    void Start() { UpdateFood(); }
+
+    public void Move(int move) {
+        i += move;
+        i %= foods.Count;
+        if (i < 0) i += foods.Count;
+        UpdateFood();
+    }
+
+    private void UpdateFood() {
+        if (food) Destroy(food);
+        food = Instantiate(foods[i], transform.position, transform.rotation, transform);
+        name.text = names[i];
+    }
+}
